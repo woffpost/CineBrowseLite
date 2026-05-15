8533450ca86f8e3f0d55e76b984068909549bca0
woffpost <woffpost@gmail.com>
2026-05-15 14:59:42 +0300

    fixed err in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
